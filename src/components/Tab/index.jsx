284024fff0fe933713bfa81bb57515437fb1ec66
Amine Zouari <zouariamine52@gmail.com>
2024-12-06 07:03:51 +0100

    test

--- src/components/Tab/index.jsx
+++ src/components/Tab/index.jsx
@@ -2,19 +2,20 @@ import "./style.css";
 
 const Tab = ({ id, panel, selected, label, onClick }) => {
   return (
-    <button
-      className="tab-button"
-      aria-label={label + "tab"}
-      id={id}
-      aria-controls={panel}
-      aria-selected={selected}
-      role="tab"
-      tabIndex={selected ? "0" : "-1"}
-      onClick={onClick}
-    >
-      <p>{label}</p>
-      {selected && <div class="tab-line"></div>}
-    </button>
+    <li role="tab">
+      <button
+        className="tab-button"
+        aria-label={label + "tab"}
+        id={id}
+        aria-controls={panel}
+        aria-selected={selected}
+        tabIndex={selected ? "0" : "-1"}
+        onClick={onClick}
+      >
+        <p>{label}</p>
+        {selected && <div class="tab-line"></div>}
+      </button>
+    </li>
   );
 };
 
