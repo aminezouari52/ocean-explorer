a465d1ed6df153efa5641f4bb1b0c65198512b1a
Amine Zouari <zouariamine52@gmail.com>
2024-12-06 07:00:47 +0100

    fix

--- src/components/NavItem/index.jsx
+++ src/components/NavItem/index.jsx
@@ -1,21 +1,13 @@
 import "./style.css";
 
 const NavItem = ({ title, href }) => {
-  const handleScrollToSection = (href) => {
-    const el = document.getElementById(href);
-    if (el) {
-      el.scrollIntoView({ behavior: "smooth" });
-      el.focus();
-    }
-  };
-
   return (
-    <li className="li-nav-item" role="tab">
+    <li className="li-nav-item">
       <a
         aria-label={title}
         className="tabs-item"
         tabIndex={0}
-        onClick={() => handleScrollToSection(href)}
+        href={`#${href}`}
       >
         <div className="tab">{title}</div>
         <div className="line"></div>
