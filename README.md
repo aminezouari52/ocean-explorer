cee6e965b3da8de4d1551b7a4908beeebd4ca4f1
Amine Zouari <zouariamine52@gmail.com>
2024-12-07 12:12:00 +0100

    feat: add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+### Project for the "nuit d'info 2024" event
+
+## task:
+
+Create a fully accessible website
--- src/components/Panel/index.jsx
+++ src/components/Panel/index.jsx
@@ -1,14 +1,16 @@
 const Panel = ({ id, labeledBy, hidden, children }) => {
   return (
-    <div
-      id={id}
-      role="tabpanel"
-      aria-labelledby={labeledBy}
-      tabIndex="-1"
-      hidden={hidden}
-    >
-      {children}
-    </div>
+    <li role="presentation">
+      <div
+        id={id}
+        role="tabpanel"
+        aria-labelledby={labeledBy}
+        tabIndex="-1"
+        hidden={hidden}
+      >
+        {children}
+      </div>
+    </li>
   );
 };
 
